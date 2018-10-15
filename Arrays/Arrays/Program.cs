63ed20c6550cec0936d412cbe275a68827540a2b
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-10-15 18:27:32 +1300

    first commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("********************Task 1*******************");
+            Console.WriteLine("*********************************************");
+
+            string[] name = new String[5];
+
+            for(int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Please enter a name to the array: ");
+                name[i] = Console.ReadLine();
+            }
+
+            //Console.WriteLine("*********************************************");
+            //Console.WriteLine("********************Task 2*******************");
+            //Console.WriteLine("*********************************************");
+
+            //int[] nums = new int[10];
+
+            //Console.WriteLine("Enter");
+
+
+            //Practise method
+
+            //int[] nums = { 20, 16, 36, 88, 4, 56, 90, 42, 51, 77,
+            //                };
+        }
+    }
+}
---
+++ RandomArrays/RandomArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomArrays
+{    
+    class Program
+    {
+        static int randMin = 1;
+        static int randMax = 100;
+        static int arraySize = 50;
+        static void Main(string[] args)
+        {
+            int[] randArray = FillArray(new int[arraySize]);
+
+            Display(randArray);
+        }
+    }
+}
