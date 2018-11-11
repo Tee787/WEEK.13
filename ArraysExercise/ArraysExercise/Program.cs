d76eb744bd04ee71ede0a1e855ae25a27da0839b
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-11-12 12:02:20 +1300

    second commit

---
+++ ArraysExercise/ArraysExercise/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysExercise
+{
+    internal class Program
+    {
+        public static int randMin = 1;
+        public static int randMax = 100;
+        public static int arraySize = 50;
+
+        public static Array array { get; private set; }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("************************************************");
+            Console.WriteLine("******************Array Exercise****************");
+            Console.WriteLine("************************************************");
+
+            int[] randArray = FillArray(new int[arraySize]);
+           
+            
+            Display(randArray);
+           
+            Array.Sort(randArray);
+
+            Display(randArray);
+
+            IndexArray(randArray);
+
+
+
+            Console.WriteLine("Press ENTER exit the application.");
+            Console.ReadLine();
+        }
+
+        private static int[] FillArray(int[] arr)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(randMin, randMax);
+            }
+            return arr;
+        }
+
+        private static void Display(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static int IndexArray(int[] index)
+        {
+            int index20 = Array.IndexOf(array, 45);
+            return Index20;
+        }
+
+    }
+    //class ArrayIndex
+    //{
+    //    public static int index;
+    //    private static Array array;
+
+    //    static int IndexArray()
+    //    {
+    //        int index33 = Array.IndexOf(array, 45);
+    //        return index33;
+    //    }
+    //}
+
+}
--- RandomArrays/RandomArrays/Program.cs
+++ RandomArrays/RandomArrays/Program.cs
@@ -16,6 +16,37 @@ namespace RandomArrays
             int[] randArray = FillArray(new int[arraySize]);
 
             Display(randArray);
+            /* Display the sorted array */
+            Console.WriteLine("******* Sorted Array *******");
+            Array.Sort(randArray);
+            Display(randArray);
+            /* Exit message */
+            Console.WriteLine("Press ENTER exit the application.");
+            Console.ReadLine();
+        }
+
+        /* Method to populate an array with random numbers */
+        static int[] FillArray(int[] arr)
+        {
+            Random rand = new Random();//Create a random to use
+            /* Using the Length property in the loop */
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(randMin, randMax);
+            }
+            return arr;//Retrun the array
+        }
+
+        /* Method to display the contents of the array */
+        static void Display(int[] arr)
+        {
+            /* Using the Length property in the loop */
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
+    
 }
